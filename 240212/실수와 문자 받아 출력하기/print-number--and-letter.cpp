79e064bd5d5bd1bc8d5e7613d4c755d92ee2e621
KAGA2023 <132111544+KAGA2023@users.noreply.github.com>
2024-02-12 17:50:56 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    char c;
+    double a,b;
+    cin>>c;
+    cin>>a>>b;
+
+    cout<<fixed;
+    cout.precision(2);
+    cout<<c<<endl<<a<<endl<<b<<endl;
+    return 0;
+}
