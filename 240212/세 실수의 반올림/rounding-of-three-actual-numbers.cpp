01d1cba5e7846572ea06e7389a3b517803645a7d
KAGA2023 <132111544+KAGA2023@users.noreply.github.com>
2024-02-12 17:45:46 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout<<fixed;
+    cout.precision(3);
+    double a,b,c;
+    cin>>a>>b>>c;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    return 0;
+}
