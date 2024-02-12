574edf624e2bd90da0a9f7a4e2224d7cbf5df3fd
KAGA2023 <132111544+KAGA2023@users.noreply.github.com>
2024-02-12 17:46:35 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout<<fixed;
+    cout.precision(3);
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a<<" "<<b<<" "<<c<<endl;
+    return 0;
+}
