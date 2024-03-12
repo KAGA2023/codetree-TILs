839b37fa7ff887b7a2e74f4a438f9d607f973f9e
KAGA2023 <132111544+KAGA2023@users.noreply.github.com>
2024-03-12 20:29:40 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Func(int N){
+    for(int i=0;i<N;i++){
+        cout<<"12345^&*()_";
+        cout<<endl;
+    }
+}
+
+int main() {
+    int N;
+    cin>>N;
+    Func(N);
+
+    return 0;
+}
