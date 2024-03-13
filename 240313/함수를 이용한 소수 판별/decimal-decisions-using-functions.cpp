22b97ca5c8450c0a069bacf7b3c6ce879de348cb
KAGA2023 <132111544+KAGA2023@users.noreply.github.com>
2024-03-13 14:30:10 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240313/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240313/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -5,10 +5,10 @@ void Func(int a,int b){
     int sum=0;
     int count=0;
     for(int i=a;i<=b;i++){
-        for(int j=2;j<i;j++){
+        for(int j=1;j<=i;j++){
             if(i%j==0) count++;
         }
-        if(count==0){
+        if(count<=2){
             sum+=i;
         }
         count=0;
