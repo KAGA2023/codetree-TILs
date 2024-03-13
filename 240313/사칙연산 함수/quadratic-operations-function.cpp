80b2e6163c589aaaa2bf7d8ee012b9517aa1f4c3
KAGA2023 <132111544+KAGA2023@users.noreply.github.com>
2024-03-13 15:25:35 +0900

    Title: 사칙연산 함수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240313/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void Plus(int a,int c){
+    cout<<a<<" + "<<c<<" = "<<a+c;
+}
+
+void Minus(int a,int c){
+    cout<<a<<" - "<<c<<" = "<<a-c;
+}
+
+void Div(int a,int c){
+    cout<<a<<" / "<<c<<" = "<<a/c;
+}
+
+void Mul(int a,int c){
+    cout<<a<<" * "<<c<<" = "<<a*c;
+}
+
+int main() {
+    int a,c;
+    char o;
+    cin>>a>>o>>c;
+    if(o=='+')
+        Plus(a,c);
+    else if(o=='-')
+        Minus(a,c);
+    else if(o=='/')
+        Div(a,c);
+    else if(o=='*')
+        Mul(a,c);
+    else
+        cout<<"false";
+
+
+    return 0;
+}
