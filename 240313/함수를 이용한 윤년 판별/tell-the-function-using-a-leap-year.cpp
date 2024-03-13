631595defc7c269de7f3a753164e7061448bb14a
KAGA2023 <132111544+KAGA2023@users.noreply.github.com>
2024-03-13 14:37:19 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 240313/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 bool Func(int y){
-    if(y%4==0) return true;
-    if(y%100==0 && y%400!=0) return true;
-    return false;
+    if(y%4!=0) return false;
+    if(y%100==0 && y%400!=0) return false;
+    return true;
 }
 
 int main() {
